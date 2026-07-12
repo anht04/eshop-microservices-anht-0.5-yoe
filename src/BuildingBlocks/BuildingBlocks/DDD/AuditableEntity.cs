9364adfd7f78bc334803a40d9b4c223a30f876ca
Nhat Anh <nhatanhtruong20@gmail.com>
2026-07-12 20:26:26 +0700

    refactor: clean up equality methods and remove unused code

--- src/BuildingBlocks/BuildingBlocks/DDD/AuditableEntity.cs
+++ src/BuildingBlocks/BuildingBlocks/DDD/AuditableEntity.cs
@@ -11,31 +11,8 @@ public abstract class AuditableEntity<T> : IEntity<T>, IHasCreationAudit, IHasMo
     public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 
     public string? CreatedBy { get; set; }
-    
+
     public DateTimeOffset? LastModified { get; set; }
-    
+
     public string? LastModifiedBy { get; set; }
-    
-    public override bool Equals(object? obj)
-    {
-        if (obj is not AuditableEntity<T> other)
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, other))
-        {
-            return true;
-        }
-
-        if (Id is null || other.Id is null)
-        {
-            return false;
-        }
-
-        return EqualityComparer<T>.Default.Equals(Id, other.Id);
-    }
-    
-    public override int GetHashCode() => Id?.GetHashCode() ?? 0;
-    
 }
