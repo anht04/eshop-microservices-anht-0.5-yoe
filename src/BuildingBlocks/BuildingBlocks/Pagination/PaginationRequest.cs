dab6f4f84638f9789d2ec7e982275f2a4b6c6a91
ANHT <anh.truongnhat@elca.vn>
2026-07-13 16:57:03 +0700

    feat: add more functionality to PaginationRequest

--- src/BuildingBlocks/BuildingBlocks/Pagination/PaginationRequest.cs
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginationRequest.cs
@@ -1,3 +1,23 @@
 namespace BuildingBlocks.Pagination;
 
-public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
+public record PaginationRequest
+{
+    public int PageIndex { get; init; }
+    public int PageSize { get; init; }
+
+    private const int MaxPageSize = 100;
+    private const int DefaultPageSize = 10;
+    private const int DefaultPageIndex = 1;
+
+    public PaginationRequest(int pageIndex = DefaultPageIndex, int pageSize = DefaultPageSize)
+    {
+        PageIndex = pageIndex < 0 ? DefaultPageIndex : pageIndex;
+
+        PageSize = pageSize switch
+        {
+            <= 0 => DefaultPageSize,
+            > MaxPageSize => MaxPageSize,
+            _ => pageSize
+        };
+    }
+}
