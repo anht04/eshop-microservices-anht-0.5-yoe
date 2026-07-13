c3ad0248a1c1947ff3ffe0e6304b059a3fb1d19d
Nhat Anh <nhatanhtruong20@gmail.com>
2026-07-13 20:41:23 +0700

    feat (catalog): add type api

---
+++ src/Services/Catalog/Catalog.API/Types/GetTypes/GetTypesEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Catalog.API.Types.GetTypes;
+
+public record GetTypesResponse(PaginatedResult<ProductType> Types);
+
+public class GetTypesEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Types", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetTypesQuery(request));
+
+                var response = result.Adapt<GetTypesResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetTypes")
+            .Produces<GetTypesResponse>()
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Types")
+            .WithSummary("Get Types");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Types/GetTypes/GetTypesHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.Pagination;
+using Catalog.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Types.GetTypes;
+
+public record GetTypesQuery(PaginationRequest Request) : IQuery<GetTypesResult>;
+
+public record GetTypesResult(PaginatedResult<ProductType> Types);
+
+public class GetTypesHandler(CatalogDbContext dbContext): IQueryHandler<GetTypesQuery, GetTypesResult>
+{
+    public async Task<GetTypesResult> Handle(GetTypesQuery query, CancellationToken cancellationToken)
+    {
+        var request = query.Request;
+        var totalCount = await dbContext.ProductTypes.CountAsync(cancellationToken: cancellationToken);
+
+        var Types = await dbContext.ProductTypes
+            .Skip((request.PageIndex - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetTypesResult(new PaginatedResult<ProductType>
+        {
+            PageIndex = request.PageIndex,
+            PageSize = request.PageSize,
+            TotalCount = totalCount,
+            Data = Types
+        });
+    }
+}
