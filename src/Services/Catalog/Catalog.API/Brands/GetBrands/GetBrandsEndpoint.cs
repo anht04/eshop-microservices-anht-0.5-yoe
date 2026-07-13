ee7b06cadde96d0594d63817622294e07c93818d
ANHT <anh.truongnhat@elca.vn>
2026-07-13 16:42:08 +0700

    feat: add get brands api

---
+++ src/Services/Catalog/Catalog.API/Brands/GetBrands/GetBrandsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Pagination;
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Brands.GetBrands;
+
+public record GetBrandsResponse(PaginatedResult<ProductBrand> Brands);
+
+public class GetBrandsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/brands", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBrandsQuery(request));
+
+                var response = result.Adapt<GetBrandsResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetBrands")
+            .Produces<GetBrandsResponse>()
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Brands")
+            .WithSummary("Get Brands");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Brands/GetBrands/GetBrandsHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.Pagination;
+using Catalog.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Brands.GetBrands;
+
+public record GetBrandsQuery(PaginationRequest Request) : IQuery<GetBrandsResult>;
+
+public record GetBrandsResult(PaginatedResult<ProductBrand> Brands);
+
+public class GetBrandsHandler(CatalogDbContext dbContext): IQueryHandler<GetBrandsQuery, GetBrandsResult>
+{
+    public async Task<GetBrandsResult> Handle(GetBrandsQuery query, CancellationToken cancellationToken)
+    {
+        var request = query.Request;
+        var totalCount = await dbContext.ProductBrands.CountAsync(cancellationToken: cancellationToken);
+
+        var brands = await dbContext.ProductBrands
+            .Skip((request.PageIndex - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetBrandsResult(new PaginatedResult<ProductBrand>
+        {
+            PageIndex = request.PageIndex,
+            PageSize = request.PageSize,
+            TotalCount = totalCount,
+            Data = brands
+        });
+    }
+}
